c94d901bafc8df660d1e101c39db532f934045fb
Adam Venord <avenord.dev@gmail.com>
2021-11-15 11:40:18 +0800

    Add seed data for superadmin user and its settings

--- src/database/migrations/create.ts
+++ src/database/migrations/create.ts
@@ -3,14 +3,49 @@
  * Run it via `npm run db:create`.
  */
 require('dotenv').config();
-
+import bcrypt from 'bcrypt';
 import models from '../models';
 
-models()
+const BCRYPT_SALT_ROUNDS = 12
+const database = models();
+database
   .sequelize.sync()
   .then(() => {
-    console.log('OK');
-    process.exit();
+    bcrypt.hash(
+      process.env.SUPERADMIN_PASSWORD,
+      BCRYPT_SALT_ROUNDS,
+    ).then((hashedPassword) => {
+      database['user'].create({
+        firstName: 'admin',
+        email: process.env.SUPERADMIN_EMAIL,
+        password: hashedPassword,
+        emailVerified: true,
+        active: true,
+        superadmin: true,
+      }).then((user) => {
+        database['settings'].create({
+          id: user.id,
+          theme: 'default',
+          createdById: user.id,
+          updatedById: user.id,
+        }).then(() => {
+          console.log('OK');
+          process.exit();
+        });
+        // .catch((error) => {
+        //   console.error(error);
+        //   process.exit(1);
+        // });
+      });
+      // .catch((error) => {
+      //   console.error(error);
+      //   process.exit(1);
+      // });
+    });
+    // .catch((error) => {
+    //   console.error(error);
+    //   process.exit(1);
+    // });
   })
   .catch((error) => {
     console.error(error);
